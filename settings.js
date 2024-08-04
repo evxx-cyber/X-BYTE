98a0c449143ec76abea8639ac1fcaddf0ca863dd
evxx-cyber <najatwadih051@gmail.com>
2024-08-04 12:22:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
 
 global.thumb = fs.readFileSync('./lib/bugs/XByte.png') //Don't change
 //////////////////////////////////////////////////////////
-const BOTNAME = 'X - B Y T E'; //BOT name
+const BOTNAME = 'X - S A A D'; //BOT name
 //////////////////////////////////////////////////////////
-const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
+const FOOTERNAME = '*Powered by Saadouch*'; //Footer text
 
 
 
@@ -37,7 +37,7 @@ const FOOTERNAME = '*Powered by TalkDrove*'; //Footer text
 //----------------OWNER NUMBER------------------------------//
 //////////////////////////////////////////////////////////////////
 
-global.owner = process.env.OWNER_NUMBER  || '923072380380' ;  //put your number without +
+global.owner = process.env.OWNER_NUMBER  || '212721518482' ;  //put your number without +
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -59,7 +59,7 @@ module.exports = { //exporting modules to use in plugins
 //----------------Session ID------------------------------//
 //////////////////////////////////////////////////////////////////
 
-SESSION_ID: process.env.SESSION_ID || '', //Paste your session ID here
+SESSION_ID: process.env.SESSION_ID || 'Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOExIeU43dG9CN0hCQXZISEhhMk5JTCtNN3JHcGQ1bTQ2dkcxaDE2eE9tVT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN3hobis3ZVhUNFpNR0NaM3hvWER6OG1ZaVd0ZXE0Z1pXaHAxelFBSXp5bz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnS3R3SjBuQmRVcStnM3B1MGVDbUZSRS9TSVA5bnhTVXdicGRyc3V1MDNZPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI0Q2ZpUDJqUnR3QmhFcWZNcXFlQldJVDd0RFZpWmdlLzVaZEdvU1JoWmswPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Im1Ed2tUMk94c3VOMnRiSTF4aUwxOWZQMDdCTlp1ZDVPMXd2dE1MRklJMWc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Inp1NGFKUGw1Rk81a0cxRGRtVnlSS1N0RW5RYTQzWm82by9YTEwxb1Nzalk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV0ZmYW84ZHltaGZLaGFlWE9JS1dSNm90OEZvMWhPNDFjL0NVMnl6RjZYYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib1ZnTjZuOEtnV25GbHlGWVpJeWhvcE5ScHBMTzBtZlRPLzZsajZzNG8xST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhpMWNCWXY3QTZ0VUpsT3VtTzBCZTJreTR5MDQ5WndTVEM3cWgyTnY1OHV3UE1LYndMeXdtYVpwSngrN1JuNjVMbEJQRFVSbXIybXBwZmZzZmtxR2d3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTY2LCJhZHZTZWNyZXRLZXkiOiJHcUI5VWR5R0NlTk5UOElDZUV2eTBxTlRYaHUxMzFhUnNBZ1ltT3VUYWVNPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiI1cmJCZlBOY1Q3U0t6cE00RDNsRnN3IiwicGhvbmVJZCI6ImUzZWM2ZjI0LTlhOGYtNDliNS05OWVlLWQwOTFhMjhmNmE0YSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ4V1IxcS9aUzF2NFRWSmtlT1ZXRjVyelRyV2s9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYk1Kc3JLY2VDbUdOYTF0SWdIKzZJbXBFbUpRPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkM5OEVWUkQzIiwibWUiOnsiaWQiOiIyMTI3MjE1MTg0ODI6OUBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLYp9mE2YXZhNmDINmF2K3ZhdivINin2YTYs9in2K/Ys1YyIEJPVCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTWEwa0s4SEVKSEN2YlVHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiaXBqa2R0aXE0cmJNMStQS2ZaTXgxSkhoOE5mRjNiQjVJMlRuVWNETG1Tdz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiVjJsUDZuTGI2SnF1SUVwbmlzZVZNUEhXVUtubGNqUzh1eHRBZnFsSnUzcm1VY0xKWk9tSFdOL0UxbVFTVmpxRUE0UFNqTkZsQnc3NjE1Mk5CeUtxQnc9PSIsImRldmljZVNpZ25hdHVyZSI6IkhGR3lPRzRZRHJXd0hvNUZ3dlltQ0F4M2EzMzQ1SnpvRmlpamFlZVU5TSs4aGRLL0R3MHhUUG9hc3A0dDlRNXFmcnlnR2VsN3h3d0hucEFvTG81dmpRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjEyNzIxNTE4NDgyOjlAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWXFZNUhiWXF1SzJ6TmZqeW4yVE1kU1I0ZkRYeGQyd2VTTms1MUhBeTVrcyJ9fV0sInBsYXRmb3JtIjoic21iYSIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMjc2OTY5NCwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFFUnEifQ==', //Paste your session ID here
 
 /////////////////////////////////////////////////////////////////
 //-------------------------------------------------------//
@@ -104,13 +104,13 @@ MODERATORS : process.env.MODERATORS === undefined ? "923072380380" : process.env
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE, //Maximum size that bot can download a file   
 //////////////////////////////////////////////////////////
 
-OWNER_NAME: process.env.OWNER_NAME || 'Hamza' , //Your name
+OWNER_NAME: process.env.OWNER_NAME || 'Saad' , //Your name
 //////////////////////////////////////////////////////////
 
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9' : process.env.POSTGRESQL_URL,
 //////////////////////////////////////////////////////////
 
-FOOTER: process.env.FOOTER || '*Powered by TalkDrove*',    
+FOOTER: process.env.FOOTER || '*Powered by Saadouch*',    
 //////////////////////////////////////////////////////////
 
 ANTI_BAD: process.env.ANTI_BAD || false  ,  //true or false
@@ -131,7 +131,7 @@ AUTO_READ:  process.env.AUTO_READ  || false  , //true or false
 AUTO_BIO:  process.env.AUTO_BIO  || false  ,        //true or false
 //////////////////////////////////////////////////////////
 
-ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,  //true or false
+ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || true  ,  //true or false
 //////////////////////////////////////////////////////////
 
 HEROKU_API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,    
@@ -149,7 +149,7 @@ BANNED_USER: process.env.BANNED_USER === undefined ? '' : process.env.BANNED_USE
 AI_MODE: process.env.AI_MODE === undefined ? 'true' : process.env.AI_MODE, //true or false
 //////////////////////////////////////////////////////////
 
-ANTI_LINK: process.env.ANTI_LINK || false  , //true or false
+ANTI_LINK: process.env.ANTI_LINK || true  , //true or false
 //////////////////////////////////////////////////////////
 
 BOT_DETECT: process.env.BOT_DETECT === undefined ? 'false' : process.env.BOT_DETECT,  //true or false    
@@ -170,7 +170,7 @@ AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,     //true or false
 AUTO_STICKER: process.env.AUTO_STICKER || false  , //true or false
 //////////////////////////////////////////////////////////
 
-WELCOME:  process.env.WELCOME  || false ,   //true or false
+WELCOME:  process.env.WELCOME  || true ,   //true or false
 //////////////////////////////////////////////////////////
 
 ANTI_DELETE : process.env.ANTI_DELETE || false , //true or false
